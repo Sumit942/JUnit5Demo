76359154d81bdb8e4a0c2921b4c9ec745e3fdc92
Sumit942 <sumitpaswan942@gmail.com>
2022-03-04 10:38:40 +0530

    The First Commit from IntelliJ

--- src/main/java/com/example/demo/A2_MultiThreadDemo.java
+++ src/main/java/com/example/demo/A2_MultiThreadDemo.java
@@ -3,11 +3,23 @@ package com.example.demo;
 public class A2_MultiThreadDemo {
 
     public static void main(String[] args) {
-        Thread t1 = new Thread(new MyThread("T1"));
-        Thread t2 = new Thread(new MyThread("T2"));
-        Thread t3 = new Thread(new MyThread("T3"));
-        Thread t4 = new Thread(new MyThread("T4"));
-        Thread t5 = new Thread(new MyThread("T5"));
+
+        Runnable r = () -> {
+            System.out.println("Running Thread: " + Thread.currentThread().getName());
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                System.out.println("Exception: Thread " + Thread.currentThread().getName());
+            }
+            System.out.println("Stopped Thread: " + Thread.currentThread().getName());
+        };
+
+        Thread t1 = new Thread(r,"T1");
+        Thread t2 = new Thread(r,"T2");
+        Thread t3 = new Thread(r, "T3");
+        Thread t4 = new Thread(r, "T4");
+        Thread t5 = new Thread(r, "T5");
+
         t1.start();
         t2.start();
         t3.start();
@@ -18,37 +30,11 @@ public class A2_MultiThreadDemo {
             t2.join();
             t3.join();
             t4.join();
-            System.out.println("All Threads finished processing!!");
+            System.out.println("All 4 Threads finished processing!!");
             t5.start();
             t5.join();
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println("Exception in Thread join: " + e.getMessage());
         }
-
-        //System.out.println("Main Thread Completed!!");
-    }
-}
-
-class MyThread implements Runnable {
-
-    private final String name;
-
-    MyThread(String name) {
-        this.name = name;
-    }
-
-    @Override
-    public void run() {
-
-        System.out.println("Running... Thread: " + name);
-        for (int i = 0; i < 3; i++) {
-            //System.out.println("Thread: " + name);
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                System.out.println("Exception in MyThread: " + name);
-            }
-        }
-        System.out.println("Stopped!! Thread: " + name);
     }
 }
